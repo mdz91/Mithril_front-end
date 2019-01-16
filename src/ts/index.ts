0ce711e63ceef61ce0059f2dff09abb0570afd49
Mourad Nasser <Mourad.Nasser@hva.nl>
2019-01-16 05:07:36 +0100

    Travis updated

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,49 +5,48 @@
  * GNU General Public Licence version 3 (GPL) version 3,
  * copied verbatim in the file "LICENSE"
  */
-import m from "mithril";
+import m from 'mithril';
 
-import "../scss/test.scss";
-import  $ from "jquery";
-import "popper.js";
-import "bootstrap";
+import '../scss/test.scss';
+import 'popper.js';
+import 'bootstrap';
 
-import { CatsCreateForm } from "./views/CreateCatform";
-import { CatsUpdateForm } from "./views/UpdateCats";
-import { CatsDeleteForm } from "./views/DeleteCat";
-import NavBar from "../component/navbar";
-import { CatsHome } from "./views/CatsHome";
-import CatList from "./views/CatDetails";
+import { CatsCreateForm } from './views/CreateCatform';
+import { CatsUpdateForm } from './views/UpdateCats';
+import { CatsDeleteForm } from './views/DeleteCat';
+import NavBar from '../component/navbar';
+import { CatsHome } from './views/CatsHome';
+import CatList from './views/CatDetails';
 
 // remove hashbang from urlprefix
-m.route.prefix("");
+m.route.prefix('');
 
-m.route(document.body, "/", {
-    "/": {
+m.route(document.body, '/', {
+    '/': {
         // tslint:disable-next-line:typedef
         render() {
             return m(NavBar, m(CatsHome));
         },
     },
-    "/cats/:id": {
+    '/cats/:id': {
         // tslint:disable-next-line:typedef
         render(vnode) {
             return m(NavBar, m(CatList, vnode.attrs));
         },
     },
-    "/create": {
+    '/create': {
         // tslint:disable-next-line:typedef
         render() {
             return m(NavBar, m(CatsCreateForm));
         },
     },
-    "/edit": {
+    '/edit': {
         // tslint:disable-next-line:typedef
         render() {
             return m(NavBar, m(CatsUpdateForm));
         },
     },
-    "/delete": {
+    '/delete': {
         // tslint:disable-next-line:typedef
         render() {
             return m(NavBar, m(CatsDeleteForm));
